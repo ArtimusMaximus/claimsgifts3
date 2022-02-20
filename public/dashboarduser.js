0a65e11bf03a97a6b488b842568893952d3b19f0
AMIV <everydamnedday@gmail.com>
2022-02-20 11:03:36 -0800

    removing add date on event card, moving it to initial event setup

--- public/dashboarduser.js
+++ public/dashboarduser.js
@@ -187,6 +187,8 @@ console.log(currentEventsArray.length);
     
 for(let i=0; i < currentEventsArray.length; i++){
     let events = currentEventsArray[i]
+    
+    
 
     const card = document.createElement('div')
     card.className = "container col-6"
@@ -197,27 +199,29 @@ for(let i=0; i < currentEventsArray.length; i++){
   <p class="card-text">${events}</p>
 </div>
 <div class="row">
-  <div class="container col-6">
-    <svg id="${events}" xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
+  <div class="container col-12">
+    <svg name="${events}" xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
       <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"/>
     </svg>
     <p>Remove</p>
   </div>
-  
-  <div class="container col-6" id="adddate">
-    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-clock" viewBox="0 0 16 16">
+  `
+
+{/* <div class="container col-6">
+    <svg id="adddate" xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-clock" viewBox="0 0 16 16">
       <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z"/>
       <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0z"/>
     </svg>
     <p>Date</p>
     </div>
-</div>
-  `
+</div> */}
+
+
 h1EventsRow.appendChild(card)
 
+// console.log(events)
 
-
-let remEvent = document.querySelectorAll(`#${events}`)
+let remEvent = document.querySelectorAll(`[name="${events}"]`)
 
 remEvent.forEach(link => link.addEventListener('click', () => {
     let arr = [];
@@ -248,44 +252,67 @@ remEvent.forEach(link => link.addEventListener('click', () => {
     
 }))
 
-}
+} //end loop
 
+// let dateId = document.querySelectorAll('#adddate')
+// console.log(typeof(dateId))
+// console.log(dateId.length)
+// console.log(dateId)
+// console.log(dateId[0])
+// console.log(dateId[0].childNodes)
+// console.log(dateId[0].childNodes[0])
+// console.log(dateId[0].childNodes[0].data)
+// console.log(dateId[0].TEXT_NODE)
+// console.log(dateId[0].baseURI)
 
+// let dateId = document.querySelectorAll('#adddate')
+// console.log(dateId)
 
+// for(let i = 0; i < dateId.length; i++){
+//     let arr1 = [];
+//     // arr1.push(dateId[i].childNodes[0].data)
+//     arr1.push(dateId[i].attributes[1].value)
+//     let events3 = dateId[i].attributes[1].value
+//     console.log('arr1 ' + arr1)
+    
+//     // let events2 = dateId[i].childNodes[0].data
+//     // console.log('events2 ' + events2)
+//     // dateId.name = `${events2}`
+    
+// // let addDate = document.querySelectorAll(`${events2}`)
 
-let addDate = document.querySelectorAll('#adddate')
-
-addDate.forEach(date => date.addEventListener('click', () => {
-    (async () => {
-        let { value: text } = await Swal.fire({
-            title: 'Set a date for your event:',
-            width: 600,
-            input: 'text',
-            inputPlaceholder: 'mm/dd/yyyy',
-            padding: '3em',
-            color: '#DC143C',
-            confirmButtonColor: '#DC143C',
-            confirmButtonText: 'Confirm',
-            background: '#fff url()',
-            backdrop: `
-              rgba(0,0,123,0.4)
-              url("/images/christmasmickeymouse.gif")
-              left top
-              no-repeat
-            `
-        })
-    if(text){
-        Swal.fire({
-            title: 'Date has been "up-Dated"!',
-            type: 'success',
-            iconColor: '#DC143C',
-            confirmButtonColor: '#DC143C',
-        })
+// dateId.forEach(date => date.addEventListener('click', () => {
+//     (async () => {
+//         let { value: text } = await Swal.fire({
+//             title: `Set a date for your event ${events3.map(item => item)}:`, //date ?
+//             width: 600,
+//             input: 'text',
+//             inputPlaceholder: 'mm/dd/yyyy',
+//             padding: '3em',
+//             color: '#DC143C',
+//             confirmButtonColor: '#DC143C',
+//             confirmButtonText: 'Confirm',
+//             background: '#fff url()',
+//             backdrop: `
+//               rgba(0,0,123,0.4)
+//               url("/images/christmasmickeymouse.gif")
+//               left top
+//               no-repeat
+//             `
+//         })
+//     if(text){
+//         Swal.fire({
+//             title: 'Date has been "up-Dated"!',
+//             type: 'success',
+//             iconColor: '#DC143C',
+//             confirmButtonColor: '#DC143C',
+//         })
         
-        console.log(text)
-    }    
+//         console.log(text)
+//     }    
 
+//     })()
 
-    })()
+// }))
 
-}))
+// } // end loop
